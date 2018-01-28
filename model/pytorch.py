a4263a42dc7943eb2b1257b771d4de80d86e0222
khurramjaved96 <14besekjaved@seecs.edu.pk>
2018-01-29 00:41:14 +0500

    Adding densenet

--- model/pytorch.py
+++ model/pytorch.py
@@ -67,18 +67,25 @@ test_loader = torch.utils.data.DataLoader(
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv1 = nn.Conv2d(3, 10, kernel_size=5,padding=(2,2))
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5,padding=(2,2))
+        self.conv2_bn1 = nn.BatchNorm2d(20)
+        self.conv3 = nn.Conv2d(20, 30, kernel_size=5,padding=(2,2))
+        self.conv2_bn2 = nn.BatchNorm2d(30)
+        self.conv4 = nn.Conv2d(30, 40, kernel_size=5,padding=(2,2))
+        self.conv2_bn3 = nn.BatchNorm2d(40)
         self.conv2_drop = nn.Dropout2d()
 
-        self.fc1 = nn.Linear(500, 50)
-        self.fc2 = nn.Linear(50, 100 )
+        self.fc1 = nn.Linear(640, 100)
+        self.fc2 = nn.Linear(100, 100 )
 
     def forward(self, x):
         x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = self.conv2_bn1(self.conv2(x))
+        x = F.relu(F.max_pool2d(self.conv2_bn2(self.conv3(x)), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2_bn3(self.conv4(x))), 2))
         # print ("X after conv", x.shape)
-        x = x.view(-1, 500)
+        x = x.view(-1, 640)
         x = F.relu(self.fc1(x))
         x = F.dropout(x, training=self.training)
         x = self.fc2(x)
@@ -89,7 +96,7 @@ model = Net()
 if args.cuda:
     model.cuda()
 
-optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+optimizer = optim.Adam(model.parameters())
 
 
 
@@ -135,7 +142,7 @@ def test(epoch=0):
     if epoch >0:
         import cv2
         img = utils.resizeImage(cMatrix.value(), 10)*255
-        cv2.imwrite("Image"+str(epoch)+".jpg", img)
+        cv2.imwrite("/output/Image"+str(epoch)+".jpg", img)
 
 
 
@@ -147,7 +154,16 @@ for epoch in range(1, args.epochs + 1):
             trainDataset.addClasses(a)
             testDataset.addClasses(a)
         for a in range(0,10):
-            trainDataset.limitClass(a,5)
+            trainDataset.limitClass(a,30)
+    if epoch == 150:
+        for a in range(20, 30):
+            trainDataset.addClasses(a)
+            testDataset.addClasses(a)
+        for a in range(10, 20):
+            trainDataset.limitClass(a, 30)
     train(epoch)
-    test(epoch)
+    if epoch%10==0:
+        test(epoch)
+    else:
+        test(epoch)
 
