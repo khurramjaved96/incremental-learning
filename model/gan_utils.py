22741b13bc85d7e5061a2a64e3663b257a01bead
Haseeb Shah <haseebs@outlook.com>
2018-02-18 22:19:14 +0500

    Added missing includes

--- model/gan_utils.py
+++ model/gan_utils.py
@@ -1,3 +1,6 @@
+import torch
+import torch.nn as nn
+
 def normal_init(m, mean, std):
     if isinstance(m, nn.ConvTranspose2d) or isinstance(m, nn.Conv2d):
         m.weight.data.normal_(mean, std)
