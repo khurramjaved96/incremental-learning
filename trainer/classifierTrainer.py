1e316f67b2c8d864ed398d9ade83c3da6d1f29af
Haseeb Shah <haseebs@outlook.com>
2018-03-01 16:14:53 +0500

    Enabled distillation loss + Cleanup of unused code

--- trainer/classifierTrainer.py
+++ trainer/classifierTrainer.py
@@ -85,7 +85,6 @@ class trainer():
 
     def insert_generated_images(self, data, target, gan_images, gan_labels, batch_size, is_C=False):
         '''
-        NOT READY FOR CLASSIFIER ON STANDARD GAN
         data: Images from data iterator
         target: Labels from data iterator
         gan_images: Generated images by GAN
@@ -93,9 +92,6 @@ class trainer():
                     images
         batch_size: Current batch_size of training iterator
         '''
-        if not is_C:
-            print("Classifier not ready for standard GAN yet")
-            assert False
         if self.args.process == 'gan' or self.args.process == 'cgan':
             if not len(gan_images) == 0:
                 if is_C:
@@ -128,16 +124,7 @@ class trainer():
             newClassesIndices = torch.squeeze(torch.nonzero((weightVector == 0)).long())
             self.optimizer.zero_grad()
 
-            if self.args.process == "gan":
-                 y_onehot = torch.FloatTensor(len(data), self.dataset.classes)
-                 if self.args.cuda:
-                     y_onehot = y_onehot.cuda()
-                 y_onehot.zero_()
-                 target.unsqueeze_(1)
-                 y_onehot.scatter_(1, target, 1)
-                 output = self.model(Variable(data))
-
-            elif len(oldClassesIndices) == 0:
+            if len(oldClassesIndices) == 0:
                 dataOldClasses = data[newClassesIndices]
                 targetsOldClasses = target[newClassesIndices]
                 target2 = targetsOldClasses
@@ -187,83 +174,3 @@ class trainer():
             correct += pred.eq(target.data.view_as(pred)).cpu().sum()
         test_loss /= len(loader.dataset)
         return 100. * correct / len(loader.dataset)
-
-
-def train(optimizer, train_loader, leftover, model, modelFixed, args, dataset, verbose=False):
-    model.train()
-
-    kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
-
-    if not args.oversampling:
-        # print (train_loader.dataset.weights)
-        train_loader = torch.utils.data.DataLoader(train_loader.dataset,
-                                                   sampler=torch.utils.data.sampler.WeightedRandomSampler(
-                                                       train_loader.dataset.weights.tolist(),
-                                                       len(
-                                                           train_loader.dataset.activeClasses) * train_loader.dataset.classSize),
-                                                   batch_size=args.batch_size,
-                                                   **kwargs)
-
-    for batch_idx, (data, target) in enumerate(train_loader):
-        if args.cuda:
-            data, target = data.cuda(), target.cuda()
-
-        weightVector = (target * 0).int()
-        for elem in leftover:
-            weightVector = weightVector + (target == elem).int()
-
-        oldClassesIndices = torch.squeeze(torch.nonzero((weightVector > 0)).long())
-        newClassesIndices = torch.squeeze(torch.nonzero((weightVector == 0)).long())
-        optimizer.zero_grad()
-
-        if len(oldClassesIndices) == 0:
-            dataOldClasses = data[newClassesIndices]
-            targetsOldClasses = target[newClassesIndices]
-            target2 = targetsOldClasses
-            dataOldClasses, target = Variable(dataOldClasses), Variable(targetsOldClasses)
-
-            output = model(dataOldClasses)
-            y_onehot = torch.FloatTensor(len(dataOldClasses), dataset.classes)
-            if args.cuda:
-                y_onehot = y_onehot.cuda()
-
-            y_onehot.zero_()
-            target2.unsqueeze_(1)
-            y_onehot.scatter_(1, target2, 1)
-
-        else:
-            y_onehot = torch.FloatTensor(len(data), dataset.classes)
-            if args.cuda:
-                y_onehot = y_onehot.cuda()
-
-            y_onehot.zero_()
-            target.unsqueeze_(1)
-            y_onehot.scatter_(1, target, 1)
-
-            output = model(Variable(data))
-            if not args.no_distill:
-                dataDis = Variable(data[oldClassesIndices])
-                outpu2 = modelFixed(dataDis)
-                y_onehot[oldClassesIndices] = outpu2.data
-
-        loss = F.binary_cross_entropy(output, Variable(y_onehot))
-        loss.backward()
-        optimizer.step()
-
-
-def test(loader, model, args):
-    model.eval()
-    test_loss = 0
-    correct = 0
-
-    for data, target in loader:
-        if args.cuda:
-            data, target = data.cuda(), target.cuda()
-        data, target = Variable(data, volatile=True), Variable(target)
-        output = model(data)
-        test_loss += F.nll_loss(output, target, size_average=False).data[0]  # sum up batch loss
-        pred = output.data.max(1, keepdim=True)[1]  # get the index of the max log-probability
-        correct += pred.eq(target.data.view_as(pred)).cpu().sum()
-
-    test_loss /= len(loader.dataset)
-    return 100. * correct / len(loader.dataset)
