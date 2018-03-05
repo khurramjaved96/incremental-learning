Reverting back to correct norm computation

model/resnet32.py
@@ -113,7 +113,7 @@ class CifarResNet(nn.Module):
         if feature:
             # print("Shape of norm vector", torch.norm(x, 2, 1).unsqueeze(1).data.cpu().numpy().shape)
             # print("Shaoe of feature vector", x.data.cpu().numpy().shape)
-            return x / torch.norm(x, 2, 2).unsqueeze(1)
+            return x / torch.norm(x, 2, 1).unsqueeze(1)
         return F.sigmoid(self.fc(x))
         # return F.softmax(self.fc(x))
 