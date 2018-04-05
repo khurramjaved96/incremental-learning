99d8c235dbdca88d247085fad346e574d147e1b3
khurramjaved96 <14besekjaved@seecs.edu.pk>
2018-04-05 19:45:58 +0500

    Sync

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -273,7 +273,7 @@ class DisguisedFoolingSampleGeneration():
             lRate = 0.000001
             optimizer = SGD([self.processed_image], lr=lRate, momentum=0.9)
 
-            for i in range(1, 2):
+            for i in range(1, 300):
                 # Process image and return variable
                 # self.processed_image = preprocess_image(self.initial_image)
                 # Define optimizer for the image
