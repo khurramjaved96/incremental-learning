c780546e1bd4287b57d11f71ae797995a472b60e
khurramjaved96 <14besekjaved@seecs.edu.pk>
2018-04-12 18:06:38 +0500

    Sync

--- model/resnet32.py
+++ model/resnet32.py
@@ -135,8 +135,10 @@ class CifarResNet(nn.Module):
         if labels:
             return F.softmax(self.fc(x)/T)
         if scale is not None:
+            x = x/scale
             temp = F.log_softmax(self.fc(x) / T)
             # print("Gets here; scaled output")
+            return temp
             return temp / scale
         return F.log_softmax(self.fc(x)/T)
 
