05e5f727340a46dd695af850afb5acbc493ce0bd
khurramjaved96 <14besekjaved@seecs.edu.pk>
2018-04-13 14:42:41 +0500

    Sync

--- runExperiment.py
+++ runExperiment.py
@@ -12,8 +12,8 @@ import plotter as plt
 import trainer
 
 parser = argparse.ArgumentParser(description='iCarl2.0')
-parser.add_argument('--batch-size', type=int, default=50, metavar='N',
-                    help='input batch size for training (default: 100)')
+parser.add_argument('--batch-size', type=int, default=35, metavar='N',
+                    help='input batch size for training (default: 35)')
 parser.add_argument('--lr', type=float, default=2.0, metavar='LR',
                     help='learning rate (default: 0.1)')
 parser.add_argument('--schedule', type=int, nargs='+', default=[23, 30, 34],
