f205d3ba4d2087ce141b3be704a1ee145778fd66
khurramjaved96 <14besekjaved@seecs.edu.pk>
2018-04-15 18:39:54 +0500

    Initialize new weights

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -166,13 +166,13 @@ class Trainer(GenericTrainer):
         for param in self.model_fixed.parameters():
             param.requires_grad = False
         self.model_fixed.eval()
-        myModel = model.ModelFactory.get_model(self.args.model_type, self.args.dataset)
-        if self.args.cuda:
-            myModel.cuda()
-        self.model = myModel
-        self.optimizer = torch.optim.SGD(self.model.parameters(), self.args.lr, momentum=self.args.momentum,
-                                    weight_decay=self.args.decay, nesterov=True)
-        self.model.eval()
+        # myModel = model.ModelFactory.get_model(self.args.model_type, self.args.dataset)
+        # if self.args.cuda:
+        #     myModel.cuda()
+        # self.model = myModel
+        # self.optimizer = torch.optim.SGD(self.model.parameters(), self.args.lr, momentum=self.args.momentum,
+        #                             weight_decay=self.args.decay, nesterov=True)
+        # self.model.eval()
 
     def train(self, epoch):
 
