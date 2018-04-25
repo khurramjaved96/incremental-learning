a1b574e3008f7d1e5b6f79f3f0438fb32bddb753
khurramjaved96 <14besekjaved@seecs.edu.pk>
2018-04-25 17:24:35 +0500

    Sync

--- combineKnowledge.py
+++ combineKnowledge.py
@@ -52,11 +52,11 @@ parser.add_argument('--alphas', type=float, nargs='+', default=[1.0], help='Weig
 parser.add_argument('--decay', type=float, default=0.00005, help='Weight decay (L2 penalty).')
 parser.add_argument('--step-size', type=int, default=10, help='How many classes to add in each increment')
 parser.add_argument('--T', type=float, default=1, help='Tempreture used for softening the targets')
-parser.add_argument('--memory-budgets', type=int,  nargs='+', default=[2000],
+parser.add_argument('--memory-budgets', type=int,  nargs='+', default=[0],
                     help='How many images can we store at max. 0 will result in fine-tuning')
 parser.add_argument('--epochs-class', type=int, default=70, help='Number of epochs for each increment')
 parser.add_argument('--dataset', default="CIFAR100", help='Dataset to be used; example CIFAR, MNIST')
-parser.add_argument('--lwf', action='store_true', default=False,
+parser.add_argument('--lwf', action='store_true', default=True,
                     help='Use learning without forgetting. Ignores memory-budget '
                          '("Learning with Forgetting," Zhizhong Li, Derek Hoiem)')
 
